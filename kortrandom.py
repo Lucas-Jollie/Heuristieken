caad0cc0b024ec2c67c1f8a634f41ce44cf74486
Lucas-Jollie <lucas_jollie@outlook.com>
2015-11-25 07:33:41 -0500

    Delete kortrandom.py

--- kortrandom.py
+++
@@ -1,8 +0,0 @@
-import random
-genomeFile = open("kortrandom.txt", 'w')
-
-for i in range(0,100):
-    random_list = random.sample(xrange(1,26),25)
-    print random_list
-    genomeFile.write(str(random_list))
-    genomeFile.write("\n")
