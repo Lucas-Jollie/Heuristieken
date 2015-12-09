041f0f039ae6c04baa0d55023ff8934a3debfb16
Lucas-Jollie <lucas_jollie@outlook.com>
2015-12-09 10:47:23 -0500

    blablapush

---
+++ Bruteforce/blaBla.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, cargo = None, prev = None, children = None):
+        self.cargo = cargo
+        self.prev = parent
+        self.children[25]
+    def __str__(self):
+        return str(self.cargo)
+        
+node = Node('''''')
+for i in range(len(genome)):
+    node = node.children[genome[i + 1]]
+        
